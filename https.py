17ef420432dbbc38b0198bfb827e1a1092c6cf58
Ronald <yornid@gmail.com>
2019-06-18 19:27:03 +0800

    update

--- https.py
+++ https.py
@@ -1,9 +1,18 @@
+import sys
 import http.server, ssl
 
-server_address = ('localhost', 4443)
+host = 'localhost'
+
+for x in sys.argv:
+  item = x.split('=')
+  if (item[0] == 'ip'):
+    host = item[1]
+
+server_address = (host, 4443)
 httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
 httpd.socket = ssl.wrap_socket(httpd.socket,
+                               keyfile="~/Documents/python-https-server/key.pem",
+                               certfile="~/Documents/python-https-server/server.pem",
                                server_side=True,
-                               certfile='~/Document/pythonHttpsServer/server.pem',
                                ssl_version=ssl.PROTOCOL_TLSv1)
 httpd.serve_forever()
