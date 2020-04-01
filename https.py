4f79190d0d06a5617ee3749b268379e4d6e4f2f9
ronald <yornid@gmail.com>
2020-04-01 11:34:00 +0800

    update

--- https.py
+++ https.py
@@ -2,28 +2,39 @@ import os
 import sys
 import http.server, ssl
 
+host = 'localhost'
+port = None
 #userPath =  os.path.expanduser("~/Documents/python-https-server")
 userPath = os.path.dirname(os.path.realpath(__file__))
-host = '10.116.105.93'
-port = 4443
-
 keyFile = userPath + "/key.pem"
 certFile = userPath + "/server.pem"
+isHttps = True
 
 for x in sys.argv:
   item = x.split('=')
-  if (item[0] == 'ip'):
+  if item[0] == '-s':
+    isHttps = False 
+  elif item[0] == 'ip':
     host = item[1]
+  elif item[0] == 'port':
+    port = int(item[1])
+
+if port is None:
+  if isHttps:
+    port = 4443
+  else:
+    port = 8080
 
 Handler = http.server.SimpleHTTPRequestHandler
 Handler.extensions_map['.wasm'] = 'application/wasm'
 server_address = (host, port)
-
 httpd = http.server.HTTPServer(server_address, Handler)
-httpd.socket = ssl.wrap_socket(httpd.socket,
-                               keyfile=keyFile,
-                               certfile=certFile,
-                               server_side=True,
-                               ssl_version=ssl.PROTOCOL_TLSv1)
+
+if isHttps == True:
+  httpd.socket = ssl.wrap_socket(httpd.socket,
+                                 keyfile=keyFile,
+                                 certfile=certFile,
+                                 server_side=True,
+                                 ssl_version=ssl.PROTOCOL_TLSv1)
 print('server ' + host + ' running at port ' + str(port))
 httpd.serve_forever()
